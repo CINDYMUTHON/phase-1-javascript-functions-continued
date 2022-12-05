116bd2b04a09c319dc839610cfd9e80e13a128be
CINDY <git@github.com:CINDYMUTHON/challenge2.git>
2022-12-05 12:41:42 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+const saturdayFun =function (activity ="roller-skate"){
+    return 'This Saturday, I want to $(activity)!';
+};
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork =function(activity ="go to the officer"){
+    return 'This Monday, I Will $(activity),';
+};
+mondayWork();
+mondayWork ("Work from home");
+
+const WrapAdjective = function(Wrapper ="*"){
+    const message = function(string ="special")}
+    return'You are ${Wrapper}${string}${wrapper}!';
+{;
+
+return message;
+};
+
+const result = WrapAdjective ("||");
+
+const emphatic =result ("a hard worker");
+
+console . log(emphatic);
+
+
+
